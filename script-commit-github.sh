0f6275a3537306792087497bc307e497cd354785
cexposit <cexposit@ull.edu.es>
2018-09-25 14:05:20 +0100

    https to git

--- script-commit-github.sh
+++ script-commit-github.sh
@@ -7,5 +7,5 @@ git add *
 git config --global user.email "cexposit@ull.edu.es"
 git config --global user.name "cexposit"
 git commit -m "${commitMessage}"
-git remote add origin https://github.com/KaizTen/kaizten-realtime-js
-git push -u origin master --force
+git remote add origin git://github.com/KaizTen/kaizten-realtime-js.git
+git push -u origin master
